0fb855a3154f0ba50241d77eeba989d0e25b27c9
Longcv <emcaoxuan22@gmail.com>
2024-06-19 11:52:21 +0700

    longcv

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,10 @@
+package hoidanit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and hỏi dân I");
+		System.out.println("Hello world with java and hỏi dân I");
+	}
+
+}
